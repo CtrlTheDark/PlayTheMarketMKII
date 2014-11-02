b124796f77606fb6143ba61a08b0c98121fc9986
ctrlthedark <ctrlthedark@gmail.com>
2014-11-02 00:01:45 -0400

    added yql

---
+++ PlayTheMarketMKII/YQL.h
@@ -0,0 +1,13 @@
+//
+//  YQL.h
+//  PlayTheMarketMKII
+//
+//  Created by Brandon Fink on 11/2/14.
+//  Copyright (c) 2014 Brandon Fink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YQL : NSObject
+
+@end
---
+++ PlayTheMarketMKII/YQL.m
@@ -0,0 +1,13 @@
+//
+//  YQL.m
+//  PlayTheMarketMKII
+//
+//  Created by Brandon Fink on 11/2/14.
+//  Copyright (c) 2014 Brandon Fink. All rights reserved.
+//
+
+#import "YQL.h"
+
+@implementation YQL
+
+@end
